c46420170613358a06fd619ee04bfd0b97e78a97
aydodo <ayllon.dorian@gmail.com>
2020-12-11 01:59:59 +0100

    Blabla, il est tard, c'est pas fini...

--- zerodapi/main.py
+++ zerodapi/main.py
@@ -118,11 +118,11 @@ def delete_aisle(aisle_id: int, db: Session = Depends(get_db)):
 
 @app.get("/products/", response_model=List[schemas.Product])
 def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    products = crud.get_aisles(db, skip=skip, limit=limit)
+    products = crud.get_products(db, skip=skip, limit=limit)
     return products
 
 
-@app.get("/product/{aisle_id}", response_model=schemas.Product)
+@app.get("/product/{product_id}", response_model=schemas.Product)
 def read_product(product_id: int, db: Session = Depends(get_db)):
     db_product = crud.get_product(db, product_id=product_id)
     if db_product is None:
